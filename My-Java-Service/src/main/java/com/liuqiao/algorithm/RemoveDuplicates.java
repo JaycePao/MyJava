880d01b2662ed46d017faa03bee2f2c1157d79cb
wutao <wutao_0710@163.com>
2023-10-30 21:31:31 +0800

    add:删除有序数组中的重复项

---
+++ My-Java-Service/src/main/java/com/liuqiao/algorithm/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package com.liuqiao.algorithm;
+
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length==0) return 0;
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+
+    public static void main(String[] args) {
+        int[] intArr = {1, 2, 3, 3};
+        int result = removeDuplicates(intArr);
+        System.out.println(result);
+    }
+}
