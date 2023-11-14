5ad89f9201937cb2e416b6f1e83e6b5e15e21404
wutao <wutao_0710@163.com>
2023-11-14 21:20:51 +0800

    add:两数之和

--- My-Java-Service/src/main/java/com/liuqiao/algorithm/TwoSum.java
+++ My-Java-Service/src/main/java/com/liuqiao/algorithm/TwoSum.java
@@ -8,32 +8,21 @@ import java.util.Map;
  * letCode 第一题 两数之和，两种解法
  */
 public class TwoSum {
-    public static void main(String[] args) throws IllegalAccessException {
-        System.out.println(Arrays.toString(twoSumSolution2(new int[]{2, 7, 11, 15}, 18)));
-    }
 
-//    private static int[] twoSumSolution1(int[] nums, int target) {
-//
-//        for (int i = 0; i < nums.length; i++) {
-//            for (int j = i + 1; j < nums.length; j++) {
-//                if (nums[i] + nums[j] == target && j != i) {
-//                    System.out.println("i=" + i + " j=" + j);
-//                    return new int[]{i, j};
-//                }
-//            }
-//        }
-//        return null;
-//    }
-
-    private static int[] twoSumSolution2(int[] nums, int target) throws IllegalAccessException {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+    public static int[] solution(int[] nums, int target) throws IllegalAccessException {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i=0;  i<nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+                return new int[]{map.get(complement), i};
             }
             map.put(nums[i], i);
         }
         throw new IllegalAccessException("");
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        int[] nums = new int[]{1, 2, 3, 4};
+        System.out.println(Arrays.toString(solution(nums, 5)));
     }
 }
