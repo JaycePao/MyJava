ff35988b17cc011581e15156fd4ef2f3f4098ba6
wutao <wutao_0710@163.com>
2023-11-16 17:00:35 +0800

    add:letcode 496

--- My-Java-Service/src/main/java/com/liuqiao/algorithm/NextGreatElement.java
+++ My-Java-Service/src/main/java/com/liuqiao/algorithm/NextGreatElement.java
@@ -9,34 +9,28 @@ import java.util.Stack;
  * letcode 496 下一个更大元素
  */
 public class NextGreatElement {
-    public static int[] nextGreaterElement(int[] findNums, int[] nums) {
-        if (findNums == null || nums == null || findNums.length == 0 || nums.length == 0){
+    public static int[] solution(int[] findNums, int[] allNums) {
+        if(findNums == null || allNums==null || findNums.length==0 || allNums.length==0) {
             return new int[]{};
         }
-
-        int[] res = new int[findNums.length];
-
-        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[findNums.length];
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
-            // 如果当前元素大于栈顶的元素
-            while (!stack.empty() && nums[i] > stack.peek()){
-                // 剔除栈顶元素，并将改元素放入map中，以栈顶的元素以及当前元素作为k&v
-                map.put(stack.pop(), nums[i]);
+        Stack<Integer> stack = new Stack<>();
+        for (int i=0; i< allNums.length; i++) {
+            if (!stack.empty() && allNums[i] > stack.peek()) {
+                map.put(stack.pop(), allNums[i]);
             }
-            // 把大数组的元素放入栈中
-            stack.push(nums[i]);
+            stack.push(allNums[i]);
         }
 
-        for (int i = 0; i < findNums.length; i++){
-            res[i] = map.getOrDefault(findNums[i], -1);
+        for (int j=0; j<findNums.length; j++) {
+            result[j] = map.getOrDefault(findNums[j], -1);
         }
 
-        return res;
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(nextGreaterElement(new int[]{2, 4, 5}, new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(solution(new int[]{2, 3}, new int[]{1, 2, 3, 4, 5})));
     }
-
 }
--- My-Java-Service/src/main/java/com/liuqiao/algorithm/TwoSum.java
+++ My-Java-Service/src/main/java/com/liuqiao/algorithm/TwoSum.java
@@ -8,22 +8,19 @@ import java.util.Map;
  * letCode 第一题 两数之和，两种解法
  */
 public class TwoSum {
-
     public static int[] solution(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i=0;  i<nums.length; i++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0;i<nums.length;i++) {
             int complement = target - nums[i];
-            if (map.containsKey(complement)) {
+            if(map.containsKey(complement)) {
                 return new int[]{map.get(complement), i};
             }
             map.put(nums[i], i);
         }
-
         return null;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4};
-        System.out.println(Arrays.toString(solution(nums, 5)));
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 4}, 7)));
     }
 }
