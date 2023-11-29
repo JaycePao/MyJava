33c5b00aac74906e67a9bfe520d82dec3a2aad67
wutao <wutao_0710@163.com>
2023-11-29 19:38:38 +0800

    sanlun

---
+++ My-Java-Service/src/main/java/com/liuqiao/algorithm/sanlun/RemoveNthFromEnd.java
@@ -0,0 +1,46 @@
+package com.liuqiao.algorithm.sanlun;
+
+import java.lang.reflect.Array;
+
+public class RemoveNthFromEnd {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; ++i) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        System.out.println(removeNthFromEnd(head, 2).next.next.val);
+    }
+}
